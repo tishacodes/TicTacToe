6b3f7f5598d5335732b31c9cb7933affd56a11b4
Tisha <tisha_h20@yahoo.com>
2022-07-07 05:29:39 -0400

    reset board

--- src/components/ResetBoard.js
+++ src/components/ResetBoard.js
@@ -1,15 +1,17 @@
 import React from 'react';
-
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import FiberNewIcon from '@mui/icons-material/FiberNew';
+//import Grid3x3Icon from '@mui/icons-material/Grid3x3';
 function ResetBoard() {
 
     return(
         <div className="reset-board-div">
             <div style={{ cursor: 'pointer' }}>
-                RESET
+                <RestartAltIcon fontSize='large'/>                
             </div>
 
             <div style={{ cursor: 'pointer' }}>
-                NEW
+                <FiberNewIcon fontSize='large'/>
             </div>
         </div>
     );

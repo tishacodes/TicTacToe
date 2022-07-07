c952a1970b2c4a3baa516eb17a40f06cadb31313
Tisha <tisha_h20@yahoo.com>
2022-07-07 07:51:47 -0400

    reset board

--- src/components/ResetBoard.js
+++ src/components/ResetBoard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import RefreshIcon from '@mui/icons-material/Refresh';
 import FiberNewIcon from '@mui/icons-material/FiberNew';
 //import Grid3x3Icon from '@mui/icons-material/Grid3x3';
 function ResetBoard() {
@@ -7,7 +7,7 @@ function ResetBoard() {
     return(
         <div className="reset-board-div">
             <div style={{ cursor: 'pointer' }}>
-                <RestartAltIcon fontSize='large'/>                
+                <RefreshIcon fontSize='large'/>                
             </div>
 
             <div style={{ cursor: 'pointer' }}>
--- src/components/TicTacToeGrid.js
+++ src/components/TicTacToeGrid.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ResetBoard from './ResetBoard';
 import ScoreBoard from './ScoreBoard';
 
 function TicTacToeGrid(props) {
+    const [playerScore, setPlayerScore] = useState(10);
+    const [computerScore, setComputerScore] = useState(20);
+    const [gridData, setGridData] = useState({
+        // 1: 'X',
+        // 2: 'O',
+        // 3: 'X',
+        // 4: '',
+        // 5: '',
+        // 6: '',
+        // 7: '',
+        // 8: '',
+        // 9: ''
+    });
     
     return(
         <div className="tic-tac-toe-main-div">
-            <ResetBoard/>
+            <ResetBoard gridData={gridData} setGridData={setGridData} />
 
             <div className = "tic-tac-toe-grid">
-                <div key = {1} className = "tic-tac-toe-grid-cell grid-cell-no-top grid-cell-no-left">X</div>
-                <div key = {2} className = "tic-tac-toe-grid-cell grid-cell-no-top">O</div>
-                <div key = {3} className = "tic-tac-toe-grid-cell grid-cell-no-top grid-cell-no-right">O</div>
-                <div key = {4} className = "tic-tac-toe-grid-cell grid-cell-no-left">O</div>
-                <div key = {5} className = "tic-tac-toe-grid-cell">X</div>
-                <div key = {6} className = "tic-tac-toe-grid-cell grid-cell-no-right">O</div>
-                <div key = {7} className = "tic-tac-toe-grid-cell grid-cell-no-bottom grid-cell-no-left">O</div>
-                <div key = {8} className = "tic-tac-toe-grid-cell grid-cell-no-bottom">O</div>
-                <div key = {9} className = "tic-tac-toe-grid-cell grid-cell-no-bottom grid-cell-no-right">X</div>
+                <div key = {1} className = "tic-tac-toe-grid-cell grid-cell-no-top grid-cell-no-left"> {gridData['1']} </div>
+                <div key = {2} className = "tic-tac-toe-grid-cell grid-cell-no-top"> {gridData['2']} </div>
+                <div key = {3} className = "tic-tac-toe-grid-cell grid-cell-no-top grid-cell-no-right"> {gridData['3']} </div>
+                <div key = {4} className = "tic-tac-toe-grid-cell grid-cell-no-left"> {gridData['4']} </div>
+                <div key = {5} className = "tic-tac-toe-grid-cell"> {gridData['5']} </div>
+                <div key = {6} className = "tic-tac-toe-grid-cell grid-cell-no-right"> {gridData['6']} </div>
+                <div key = {7} className = "tic-tac-toe-grid-cell grid-cell-no-bottom grid-cell-no-left"> {gridData['7']} </div>
+                <div key = {8} className = "tic-tac-toe-grid-cell grid-cell-no-bottom"> {gridData['8']} </div>
+                <div key = {9} className = "tic-tac-toe-grid-cell grid-cell-no-bottom grid-cell-no-right"> {gridData['9']} </div>
             </div>
 
-            <ScoreBoard playerScore={10} computerScore={20}/> 
+            <ScoreBoard playerScore={playerScore} 
+                        computerScore={computerScore} 
+                        setPlayerScore={setPlayerScore} 
+                        setComputerScore={setComputerScore}
+            /> 
         </div>
     );
 }

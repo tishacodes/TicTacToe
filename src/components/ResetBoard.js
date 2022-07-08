fc7cc9e37e9f386073601ce43802fb8e98c86056
Tisha <tisha_h20@yahoo.com>
2022-07-08 16:36:18 -0400

    mui icons

--- src/components/ResetBoard.js
+++ src/components/ResetBoard.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { IconButton } from "@material-ui/core";
 import RefreshIcon from '@mui/icons-material/Refresh';
 import FiberNewIcon from '@mui/icons-material/FiberNew';
+//import CancelIcon from '@material-ui/icons/Cancel';
+
 
 function ResetBoard(props) {
 
     const refreshScoreBoard = (event) => {  
         event.preventDefault();         
-        props.setPlayerScore(0);
+        props.setPlayerScore(55);
         props.setComputerScore(0);
     }
 
@@ -15,16 +18,25 @@ function ResetBoard(props) {
         props.setGridData({});
     }
 
+//     <div role="heading" aria-level="1" aria-label="tic tac toe game header with reset controls" className="reset-board-div">
+//     <button disabled style={{ border: 0, padding: 0 }} onClick={(event) => refreshScoreBoard(event)}>
+//         <RefreshIcon component="button" sx={{ fontSize: 40 }} />                
+//     </button>
+
+//     <button className="new-button" disabled style={{ cursor: 'pointer', border: 0, padding: 0, backgroundColor: '#282c34' }} onClick={(event) => refreshGameBoard(event)}>
+//         <FiberNewIcon sx={{ fontSize: 40 }}  />
+//     </button>
+// </div>
+
     return(
         <div role="heading" aria-level="1" aria-label="tic tac toe game header with reset controls" className="reset-board-div">
-            <div style={{ cursor: 'pointer' }} onClick={(event) => refreshScoreBoard(event)}>
-                {/* <RefreshIcon fontSize='large'/>                 */}
-                <p>TEST</p>
-            </div>
+            <IconButton disabled aria-label="reset scores" color="inherit" onClick={(event) => refreshScoreBoard(event)}>
+                <RefreshIcon fontSize="large" color="inherit" style={{ cursor: 'pointer', margin: 0 }}/>                
+            </IconButton>
 
-            <div style={{ cursor: 'pointer' }} onClick={(event) => refreshGameBoard(event)}>
-                <FiberNewIcon fontSize='large'/>
-            </div>
+            <IconButton disabled aria-label="reset current game" color="inherit" onClick={(event) => refreshGameBoard(event)}>
+                <FiberNewIcon fontSize="large" color="inherit" style={{ cursor: 'pointer' }}/>
+            </IconButton>
         </div>
     );
 }

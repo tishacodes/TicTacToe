285fd87acf327751d0e2db1813f53621cf60d875
Tisha <tisha_h20@yahoo.com>
2022-07-13 09:45:45 -0400

    tic tac toe grid

--- src/components/TicTacToeGrid.js
+++ src/components/TicTacToeGrid.js
@@ -20,6 +20,8 @@ function TicTacToeGrid(props) {
         // 8: '',
         // 9: ''
     });
+
+    const answerArr = [ [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7] ];
     
     const gridCellOnClick = (event) => {        
         //console.log('on click', event);
